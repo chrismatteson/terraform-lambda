eb79456b1e3ecb17412daf867362a4dbce8e04f2
Chris Matteson <chris.matteson@gmail.com>
2019-07-17 14:13:22 -0700

    add output

--- main.tf
+++ main.tf
@@ -11,11 +11,31 @@ data "archive_file" "lambda" {
   }
 }
 
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "${random_id.project_name.hex}-iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "vault_lambda" {
   count            = var.enable ? 1 : 0
   filename         = data.archive_file.lambda.output_path
   function_name    = var.function_name
-  role             = var.aws_iam_role
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "lambda_function.lambda_handler"
   source_code_hash = "${base64sha256(file("${data.archive_file.lambda.output_path}"))}"
   runtime          = var.runtime
