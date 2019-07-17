a0d3277fde5487c5bace6e507963335a65a865fa
Chris Matteson <chris.matteson@gmail.com>
2019-07-17 14:02:33 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  output_path = "${path.module}/lambda.zip"
+
+  dynamic "source" {
+    for_each = var.source_files
+    content {
+      filename = subnet.key
+      content  = subnet.value
+    }
+  }
+}
+
+resource "aws_lambda_function" "vault_lambda" {
+  filename         = data.archive_file.lambda.output_path
+  function_name    = var.function_name
+  role             = var.aws_iam_role
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = "${base64sha256(file("${data.archive_file.lambda.output_path}"))}"
+  runtime          = var.runtime
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  tags             = var.tags
+
+  environment {
+    variables = {
+      for key in var.environment_variables :
+      key => key.value
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "function_name" {}
+variable "source_files" {}
+variable "aws_iam_role" {}
+variable "runtime" {
+  default = python2.7
+}
+variable "timeout" {
+  default = 30
+}
+variable "memory_size" {
+  default = 128
+}
+variable "tags" {}
+variable "environment_variables" {}
